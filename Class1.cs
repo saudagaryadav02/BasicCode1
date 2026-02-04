4dd6c8c4fe936de3e483dd5eee4230f2bd29347e
saudagaryadav02 <saudagaryadav02@gmail.com>
2026-02-04 17:28:02 +0530

    test

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿namespace BasicCodes
+{
+    internal class Guess
+    {
+        public static void Secret()
+        {
+            int a = 0;
+            int secret = 24;
+
+            Random rnd = new Random();
+            a = rnd.Next(1, 10);
+
+            while (true)
+            {
+
+                Console.WriteLine("Enter the value for a:");
+                a = Convert.ToInt32(Console.ReadLine());
+
+
+                if (a != secret)
+                {
+                    if (a < secret)
+                    {
+                        Console.WriteLine("Too Low");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too High");
+
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Correct");
+                    break;
+
+                }
+            }
+        }
+    }
+}
