77a9ef3c86079341d0000af28cf9920ef144e91b
saudagaryadav02 <saudagaryadav02@gmail.com>
2026-02-04 17:35:55 +0530

    Reverse any number
    Take input of number  from user and  Reverse it .

---
+++ Reverse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCodes
+{
+    internal class Reverse
+    {
+        public static void rev()
+        {
+            int l;
+            Console.WriteLine("enter the lenth");
+            l = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[l];
+
+            for (int i=0;i<arr.Length;i++)
+            {
+                Console.WriteLine("Enter number " + (i + 1));
+                arr[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0,j=arr.Length-1; i < j; i++,j--)
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+            Console.WriteLine("your revesed numbers are :");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
